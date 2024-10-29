6146984591e4b850cb45b41272b2824fb7255f1d
JLee <xiaoyaodx@hotmail.com>
2024-10-29 09:19:18 +0800

    feat: config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  printWidth: 100,
+  semi: true, // Add semicolons at the ends of statements
+  singleQuote: true, // Use single quotes instead of double quotes
+  printWidth: 100, // Line length that the formatter will wrap on
+  trailingComma: 'all', // Add trailing commas in object and array literals
+  tabWidth: 2, // Number of spaces per indentation level
+  useTabs: false, // Use spaces for indentation
+  bracketSpacing: true, // Add spaces between brackets in object literals
+  arrowParens: 'always', // Include parentheses around a sole arrow function parameter
 };
